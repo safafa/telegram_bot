8e6a409b5789cd326e2975142a1742dbd78096f2
Safa <safaaballagh97@gmail.com>
2021-05-10 08:52:27 +0000

    Add test for initialize Bot

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,7 +1,16 @@
-include_relative '../lib/bot'
+require_relative '../lib/bot'
 require 'dotenv'
 Dotenv.load
 
 describe Bot do
-  let(:bot) { Bot.new }
+  let(:bot) { Bot.new(ENV['BOT_KEY']) }
+
+  describe '.initialize' do
+    it 'initialize a the token variable for a new bot' do
+      expect(bot).to be_instance_of(Bot)
+    end
+    it 'Throw error when no argument is given' do
+      expect { bot.new }.to raise_error
+    end
+  end
 end
