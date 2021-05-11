15d7706fb589e5e43b7178306a8620f1666eab0b
Safa <safaaballagh97@gmail.com>
2021-05-11 23:14:47 +0000

    Fix bot rspec test

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -4,11 +4,12 @@ describe Bot do
   let(:bot) { Bot.new('1814035314:AAEJA9-HRNunFeaI5UxGV4KBV6LUBSK8v6g') }
   let(:telegram) { bot.telegram_bot }
   describe '.initialize' do
+    BOTKEY = '1814035314:AAEJA9-HRNunFeaI5UxGV4KBV6LUBSK8v6g'
     it 'instantiate Bot class' do
       expect(bot).to be_instance_of(Bot)
     end
     it 'initialize  a the token variable for a new bot' do
-      expect(bot.token).to eq ENV['BOT_KEY']
+      expect(bot.token).to eq BOTKEY
     end
     it 'Throw error when no argument is given' do
       expect { Bot.new }.to raise_error
